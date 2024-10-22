299d597bdda66ed1d28a664c68432a08cfc94a00
mustafaprogramming <mustafaghaffar763@gmail.com>
2024-10-22 21:17:34 +0500

    AniSphere-Theme-Remember

--- src/app/GlobalContext.jsx
+++ src/app/GlobalContext.jsx
@@ -1,17 +1,17 @@
 'use client'
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+
 
 const globalContext=createContext({})
 export const useGlobalContext=()=>useContext(globalContext)
 
 const GlobalContext = ({data,children}) => {
   const [NameType,setNameType]=useState(true);
-  const [theme,setTheme]=useState(
-    ()=>JSON.parse(localStorage.getItem('theme'))
-    ||
-    'Default'
-  );
-
+  const [theme,setTheme]=useState('Default');
+  useEffect(()=>{
+    let value = JSON.parse(localStorage.getItem('theme')) || 'Default'
+    setTheme(value)
+  },[]);
   const toggleName=()=>setNameType(!NameType)
   return (
     <globalContext.Provider value={{...data,NameType,toggleName,theme,setTheme}}>
